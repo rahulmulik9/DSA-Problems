4f3bb6c6b178c5a525a19465c80172303be3a034
rahulmulik9 <rahulmulik9@gmail.com>
2022-11-05 13:42:25 +0530

    Solved Problems Raja Softwear lab Practice FindDuplicateinList

--- src/RAJA/FindDuplicateinList.java
+++ src/RAJA/FindDuplicateinList.java
@@ -1,5 +1,38 @@
 package RAJA;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FindDuplicateinList {
 
+    public static void removeDuplicates(ArrayList<Integer> arlst){
+        ArrayList<Integer> arls=new ArrayList<Integer>();
+
+        for (int a:arlst) {
+            if(!arls.contains(a))
+                arls.add(a);
+        }
+      for(int a:arls){
+          System.out.print(a);
+      }
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> arlst=new ArrayList<Integer>();
+        Scanner sc=new Scanner(System.in);
+        boolean stop=true;
+
+        while (stop){
+            System.out.println("Enter elements");
+            int a= sc.nextInt();
+            if(a != 0)arlst.add(a);
+            else stop=false;
+        }
+
+        removeDuplicates(arlst);
+
+
+    }
+
 }
+
